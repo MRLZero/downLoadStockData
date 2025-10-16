11db05da78a6e62941c079fdc026830b6a4d98aa
MRLZero <lvchalc@163.com>
2025-10-16 17:00:21 +0800

    update

--- code/getJPHistoryData.py
+++ code/getJPHistoryData.py
@@ -6,7 +6,7 @@ def getJPHistoryData(code):
     # 获取日本数据（'.T'对应日本）
     code = str(code).strip() + '.T'
     end = time.strftime("%Y-%m-%d", time.localtime(time.time()))
-    df = yf.download(code, start="2000-01-01", end=end, auto_adjust=True, multi_level_index=False)
+    df = yf.download(code, start="2010-01-01", end=end, auto_adjust=True, multi_level_index=False)
     df.reset_index(inplace=True)
     new_df = df[['Date', 'Close']]
     new_df.columns = ['净值日期', '累计净值']
