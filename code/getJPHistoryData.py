5a78bba89bcab647e9b499d87af6280239410a90
MRLZero <lvchalc@163.com>
2025-10-16 16:48:38 +0800

    update

--- code/getJPHistoryData.py
+++ code/getJPHistoryData.py
@@ -6,7 +6,7 @@ def getJPHistoryData(code):
     # 获取日本数据（'.T'对应日本）
     code = str(code).strip() + '.T'
     end = time.strftime("%Y-%m-%d", time.localtime(time.time()))
-    df = yf.download(code, start="2015-01-01", end=end, auto_adjust=True, multi_level_index=False)
+    df = yf.download(code, start="2000-01-01", end=end, auto_adjust=True, multi_level_index=False)
     df.reset_index(inplace=True)
     new_df = df[['Date', 'Close']]
     new_df.columns = ['净值日期', '累计净值']
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     save_path = 'data/fundsHistoryValue'
     for code in codes:
         df = getJPHistoryData(code)
+        df = process_qfq_value(df)
         saveFundHistoryValues(code, df, save_path)
 
 
